563d03aefd7ec1407911b22be305db71de999ed8
valentin <valentin@brueckel.org>
2023-06-05 08:01:06 +0200

    initial

---
+++ src/main/java/de/vbbn/helloaction/HelloActionApplication.java
@@ -0,0 +1,13 @@
+package de.vbbn.helloaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/vbbn/helloaction/HelloActionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vbbn.helloaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
